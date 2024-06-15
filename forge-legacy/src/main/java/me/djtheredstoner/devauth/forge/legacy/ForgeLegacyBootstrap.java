ca625eb80bac044f082b0dafa73b1207aa932e96
DJtheRedstoner <52044242+DJtheRedstoner@users.noreply.github.com>
2024-06-15 11:51:11 -0400

    Fix DevAuth being able to be called twice on legacy forge
    Closes #11

--- forge-legacy/src/main/java/me/djtheredstoner/devauth/forge/legacy/ForgeLegacyBootstrap.java
+++ forge-legacy/src/main/java/me/djtheredstoner/devauth/forge/legacy/ForgeLegacyBootstrap.java
@@ -1,13 +1,25 @@
 package me.djtheredstoner.devauth.forge.legacy;
 
 import me.djtheredstoner.devauth.common.DevAuth;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 public class ForgeLegacyBootstrap {
 
+    private static final Logger LOGGER = LogManager.getLogger("DevAuth/Bootstrap");
+
     private static final DevAuth devAuth = new DevAuth();
 
+    private static boolean loggedIn = false;
+
     public static String[] processArguments(String[] args) {
-        return devAuth.processArguments(args);
+        if (!loggedIn) {
+            loggedIn = true;
+            return devAuth.processArguments(args);
+        } else {
+            LOGGER.warn("DevAuth called twice, you may have loaded DevAuth twice (eg. from a jar and from maven).");
+            return args;
+        }
     }
 
 }
